0cfe3a3a6f79e3e39b2b6bedd4d8ad9e87078af8
Roman Marchant <roman.marchant@sydney.edu.au>
2021-01-20 15:51:35 +1100

    Example hello world uses bn library.

--- src/bn/examples/example_hello_bn.cpp
+++ src/bn/examples/example_hello_bn.cpp
@@ -1,10 +1,12 @@
 // "Hello World!" example program
 //
 
+#include <bn/bn/graph.h>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+    bn::Graph graph;
     return 0;
 }
