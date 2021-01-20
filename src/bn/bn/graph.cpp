0b85d101660439309007c2ae74ded60f52ca1fb1
Roman Marchant <roman.marchant@sydney.edu.au>
2021-01-20 15:42:23 +1100

    Compiled library generated

---
+++ src/bn/bn/graph.cpp
@@ -0,0 +1,12 @@
+#include <bn/bn/graph.h>
+#include <iostream>
+
+namespace bn
+{
+
+Graph::Graph()
+{
+    std::cout<<"Created Graph"<<std::endl;
+}
+
+}
---
+++ src/bn/bn/graph.h
@@ -0,0 +1,15 @@
+#ifndef __BN_GRAPH_H__
+#define __BN_GRAPH_H__
+
+namespace bn
+{
+
+class Graph
+{
+    public:
+        Graph();
+};
+
+}
+
+#endif /* __BN_GRAPH_H__ */
